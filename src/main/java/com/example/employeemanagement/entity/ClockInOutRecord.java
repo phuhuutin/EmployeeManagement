cf56be57287b8ef0621b4a9875a4572b99fdea35
phuhuutin <39242538+phuhuutin@users.noreply.github.com>
2024-10-09 22:10:00 -0700

    added ClockInOutRecord

--- src/main/java/com/example/employeemanagement/entity/ClockInOutRecord.java
+++ src/main/java/com/example/employeemanagement/entity/ClockInOutRecord.java
@@ -1,10 +1,17 @@
 package com.example.employeemanagement.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Data // Generates getters, setters, toString, equals, and hashCode methods
+@NoArgsConstructor // Generates a no-args constructor
+@AllArgsConstructor // Generates an all-args constructor
 public class ClockInOutRecord {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,5 +26,10 @@ public class ClockInOutRecord {
 
     @ManyToOne
     @JoinColumn(name = "shift_id")
+    @JsonIgnore
     private Shift shift;
+
+    public double getMinuteWorked(){
+        return java.time.Duration.between(this.getClockInTime(), this.getClockOutTime()).toMinutes();
+    }
 }
