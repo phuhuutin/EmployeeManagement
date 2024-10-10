fbefa51b06b0b15bf4e3ee5088d73619aed28257
phuhuutin <39242538+phuhuutin@users.noreply.github.com>
2024-10-09 22:09:09 -0700

    added another constructor

--- src/main/java/com/example/employeemanagement/entity/AttendanceReason.java
+++ src/main/java/com/example/employeemanagement/entity/AttendanceReason.java
@@ -1,12 +1,14 @@
 package com.example.employeemanagement.entity;
 
+
 public enum AttendanceReason {
     LATE(-1, "Late"),           // 1 point deduction for being late
     ABSENT(-2, "Absent"),       // 2 points deduction for being absent
-    ON_TIME(0, "On Time");      // 0 points for being on time
+    LEAVEEARLY(-1, "Leave Early");      // 0 points for being on time
 
     private final int points;
     private final String name;
+
 
     AttendanceReason(int points, String name) {
         this.points = points;
