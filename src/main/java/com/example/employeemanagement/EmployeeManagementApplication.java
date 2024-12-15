3a0829fc74bf36c712a0121059855e32afb7316b
phuhuutin <39242538+phuhuutin@users.noreply.github.com>
2024-12-15 13:14:17 -0800

    updated

--- src/main/java/com/example/employeemanagement/EmployeeManagementApplication.java
+++ src/main/java/com/example/employeemanagement/EmployeeManagementApplication.java
@@ -1,7 +1,14 @@
 package com.example.employeemanagement;
 
+import org.jobrunr.configuration.JobRunr;
+import org.jobrunr.scheduling.JobScheduler;
+import org.jobrunr.server.JobActivator;
+import org.jobrunr.storage.sql.common.SqlStorageProviderFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class EmployeeManagementApplication {
@@ -9,5 +16,4 @@ public class EmployeeManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeManagementApplication.class, args);
     }
-
 }
